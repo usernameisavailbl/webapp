0f39894127460a6e39e535d6844eff117cfb07c0
“Natdanai <champpp008@gmail.com>
2025-04-03 20:32:11 +0700

    change font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
+import Image from 'next/image'
+import { Bungee_Tint } from "next/font/google";
+
+const bungee = Bungee_Tint({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 export default function MyApp() {
-  return (<div className="w-[80%] border rounded w-100vw ml-20 p-2">
-    <h1 className="text-2xl font-bold m-2 p-2">Hello world!!</h1>
-    <p className="border rounded m-2 p-2 w-100 bg-blue-100 text-black">
+  return (<div className="w-[80%] border rounded w-100vw ml-20 p-2
+    grid
+    grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-4">
+
+    <h1 className={`${bungee.className} "font-bold m-2 p-2"`}>Hello world!!</h1>
+
+    <Image
+      className='p-2 m-4'
+      src="https://computing.psu.ac.th/th/wp-content/uploads/2023/09/PSU-CoC-ENG_01_x49.png"
+      width={150}
+      height={150}
+      alt="PSU Logo"
+    />
+
+    <Image
+      className='p-2 m-2'
+      src="/jpstreet.avif"
+      width={250}
+      height={250}
+      alt="Picture of the author"/>
+
+    <p className="border rounded m-2 p-2 w-100 bg-blue-100 text-black font">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illum eum ab sed iusto ullam tempore necessitatibus non esse illo.
     </p>
     <p className="border rounded m-2 p-2 w-100 bg-pink-100 text-black">
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'computing.psu.ac.th',
+        pathname: '/**',
+      }
+    ]
+  },
 };
 
 export default nextConfig;
