f107769db01ec68b10a6e526bbce9a3c8eec2f08
“Natdanai <champpp008@gmail.com>
2025-04-03 10:55:12 +0700

    chenge text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 export default function MyApp() {
-  return (<div>
-    <h1>Hello world!!</h1>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, aliquam.
+  return (<div className="w-[80%] border rounded w-100vw ml-20 p-2">
+    <h1 className="text-2xl font-bold m-2 p-2">Hello world!!</h1>
+    <p className="border rounded m-2 p-2 w-100 bg-blue-100 text-black">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae illum eum ab sed iusto ullam tempore necessitatibus non esse illo.
+    </p>
+    <p className="border rounded m-2 p-2 w-100 bg-pink-100 text-black">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem iure doloremque ex blanditiis totam consequatur sed provident doloribus. Dolorum nostrum dignissimos excepturi eius vitae accusamus! Illum quas ullam explicabo sed.
+    </p>
+    <p className="border rounded m-2 p-2 w-100 bg-purple-100 text-black">
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil accusamus eaque natus placeat quod nostrum aspernatur vel assumenda exercitationem quas inventore aperiam quis repudiandae harum, corporis minus esse a. Hic!
     </p>
   </div>
   )
